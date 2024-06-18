1ec428c4c8c487c1c0f3b249454e7630d4ca77e6
aniladari501@gmail.com <anil@suyay.com.au>
2024-06-18 15:23:59 +0530

    added

--- src/app.js
+++ src/app.js
@@ -62,9 +62,25 @@ passport.use('jwt', jwtStrategy);
 if (config.env === 'production') {
   app.use('/v1/auth', authLimiter);
 }
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
 // v1 API routes
-app.use('/v1', routes);
+app.use('/v1', allowCors(routes));
 // app.use('/ui', express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, '../public')));
